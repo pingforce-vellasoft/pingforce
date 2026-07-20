400d731c5a00f16305ba13cbfc3966a55160cd59
pingforce-vellasoft <pingforce.vellasoft@gmail.com>
2026-07-20 23:41:06 +0530

    fix(website): update privacy policy component metadata

--- apps/website/src/app/pages/privacy/privacy.component.ts
+++ apps/website/src/app/pages/privacy/privacy.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-privacy',
+  selector: 'pf-privacy',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './privacy.component.html',
